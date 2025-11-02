a29a3b6a83d18a1422142a5d0443ca4cf3e8220d
parateja <parateja576@gmail.com>
2025-11-02 08:07:31 +0530

    Dom style files are added

---
+++ JAVASCRIPT/Dom styles.js
@@ -0,0 +1,9 @@
+let div2=document.getElementById('two')
+div2.style.backgroundColor='yellow'
+div2.style.color ='orange'
+
+let div3=document.querySelector('.three')
+Object.assign(div3.style,{
+    backgroundColor:'yellow',
+    color:'red'
+})
